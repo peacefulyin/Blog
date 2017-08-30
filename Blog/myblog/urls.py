0709083ff1f7ddea3b9aa829399b99846c54df53
Monkey <737169168@qq.com>
2017-08-29 23:03:06 -0700

    ajax-finish

--- Blog/myblog/urls.py
+++ Blog/myblog/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import url
 from myblog import views
 
 urlpatterns = [
-    url(r'^index/(\d+)', views.index),
+    url(r'^index/', views.index),
     url(r'^text/$',views.text),
     url(r'^test/$', views.test),
-    url(r'^json/$', views.getjson),
+    url(r'^getjson/(\d+)$', views.return_json),
     url(r'^about/$',views.about),
     url(r'^artical/(.*)$',views.show_artical),
     ]
--- Blog/myblog/views.py
+++ Blog/myblog/views.py
@@ -6,14 +6,40 @@ from django.http import JsonResponse
 
 from django.shortcuts import render
 
+"""
 def index(request,pagenum):
     user = User.objects.all()
     user1 = User.objects.get(name='yin')
     artical = user1.artical_set.get(pk=1)
-    pagenator = Paginator(user,7)
+    pagenator = Paginator(user,6)
     page = pagenator.page(int(pagenum))
     context = {'data':page,'artical':artical}
     return render(request,'myblog/index.html',context)
+"""
+
+def index(request):
+    user = User.objects.all()
+    user1 = User.objects.get(name='yin')
+    artical = user1.artical_set.get(pk=1)
+    pagenator = Paginator(user,3)
+    page = pagenator.page(int(1))
+    context = {'data':page,'artical':artical}
+    return render(request,'myblog/index.html',context)
+
+
+def return_json(request,pagenum):
+    user = User.objects.all()
+    user1 = User.objects.get(name='yin')
+    artical = user1.artical_set.get(pk=1)
+    pagenator = Paginator(user, 3)
+    page = pagenator.page(int(pagenum))
+    context = {'data': page, 'artical': artical}
+    list = []
+    for i in page:
+        list.append(i.name)
+    dict = {'a':list,'b':artical.content}
+    #dict = json.dumps(dict)
+    return JsonResponse(dict)
 
 def text(request):
     return render(request,'myblog/text.html')
@@ -29,5 +55,3 @@ def show_artical(request,id):
 def test(request):
     return render(request,'myblog/comment.html')
 
-def getjson(request):
-    return JsonResponse({'data':'1'})
