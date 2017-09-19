ec34cb43667e06621b9467900d284f6bab0bd639
Monkey <737169168@qq.com>
2017-09-18 20:51:32 -0700

    a lots of things done

--- Piano/Piano/settings.py
+++ Piano/Piano/settings.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 """
 Django settings for Piano project.
 
@@ -69,6 +70,19 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Piano.wsgi.application'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.qq.com'
+EMAIL_PORT = 465
+
+#发送邮件的邮箱
+EMAIL_HOST_USER = '731769168@qq.com'
+#在邮箱中设置的客户端授权密码
+EMAIL_HOST_PASSWORD = 'JohnSnow@321.com'
+#收件人看到的发件人
+EMAIL_FROM = 'Xpiano'
+
+
 
 
 # Database
