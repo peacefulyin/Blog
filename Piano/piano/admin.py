644e365651886cb764214fe6fdf390b23e99c197
Monkey <737169168@qq.com>
2017-09-20 11:38:54 -0700

    test_ignore

--- Piano/piano/admin.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.contrib import admin
-from piano.models import *
-
-admin.site.register(User)
-admin.site.register(Sheet)
-admin.site.register(Comment)
